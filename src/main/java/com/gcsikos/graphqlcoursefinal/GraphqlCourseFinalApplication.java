023941ffea3ef6bbcf47e4b8d7112f1dbae99475
gcsikos <csiki123@gmail.com>
2022-05-03 21:15:40 +0200

    First commit

---
+++ src/main/java/com/gcsikos/graphqlcoursefinal/GraphqlCourseFinalApplication.java
@@ -0,0 +1,13 @@
+package com.gcsikos.graphqlcoursefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlCourseFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlCourseFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcsikos/graphqlcoursefinal/GraphqlCourseFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcsikos.graphqlcoursefinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlCourseFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
